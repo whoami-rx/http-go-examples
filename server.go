f031a45e33640dff90cf431eee3491e0a5363b96
RAR <mem.leaks12@gmail.com>
2019-05-17 16:50:56 -0300

    custom http handler

--- server.go
+++ server.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// ErrorHandler custom http handler
+type ErrorHandler struct {
+	Message string
+	Status  int
+}
+
+func (e ErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(e.Status)
+	fmt.Fprintf(w, "Message: %s\nStatus: %d", e.Message, e.Status)
+}
 
 func main() {
-	fmt.Println("Hello, Gold!")
+	e := ErrorHandler{Message: "Custom NOT FOUND ERROR", Status: 404}
+	m := http.NewServeMux()
+	m.Handle("/", e)
+	log.Println("Server listen at :8080")
+	log.Fatal(http.ListenAndServe(":8080", m))
 }
