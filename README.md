36b67861290d79fa1c34c6b9a1ec7f0179a03dd3
RAR <mem.leaks12@gmail.com>
2019-05-17 18:14:19 -0300

    handle different http methods

---
+++ README.md
@@ -0,0 +1,16 @@
+
+## Handle different http methods
+
+### GET
+
+```
+curl http://localohst:8080/users
+```
+
+### POST 
+
+```
+curl http://localost:8080/users -X POST
+```
+
+Other methods not implemented(return `MethodNotAllowed`)
--- server.go
+++ server.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+func usersHandle(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		w.Write([]byte("GET method"))
+	case http.MethodPost:
+		w.Write([]byte("POST method"))
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Try Again. Method Not Allowed"))
+	}
+}
 
 func main() {
-	fmt.Println("Hello, Gold!")
+	m := http.NewServeMux()
+	m.HandleFunc("/users", usersHandle)
+	http.ListenAndServe(":8080", m)
 }
