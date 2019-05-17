75d994026c7528a1d3df3f5d79bd452d32bb846d
RAR <mem.leaks12@gmail.com>
2019-05-17 16:21:15 -0300

    server http with ServerMux(multiplexer)

--- server.go
+++ server.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello From %q", html.EscapeString(r.URL.Path))
+}
 
 func main() {
-	fmt.Println("Hello, Gold!")
+	m := http.NewServeMux()
+	m.HandleFunc("/", helloWorld)
+	m.HandleFunc("/other/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Oter path")
+	})
+	log.Println("Server running at :8080")
+	log.Fatal(http.ListenAndServe(":8080", m))
 }
