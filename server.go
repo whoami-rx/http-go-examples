e27c122fb7525bf992c25ce2cee5e87cdfe4f253
RAR <mem.leaks12@gmail.com>
2019-05-17 17:13:07 -0300

    Custom http server

--- server.go
+++ server.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, Gold!")
+	m := http.NewServeMux()
+	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	server := &http.Server{
+
+		Addr:           ":8080",
+		Handler:        m,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Println("Server listen at :8080")
+	log.Fatal(server.ListenAndServe())
 }
