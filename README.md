cf2dbfe4e4bcb07e9da2b4b80381e8cd2b7d526e
RAR <mem.leaks12@gmail.com>
2019-05-17 22:13:15 -0300

    json response

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-## Golang/http examples
+## json
 
-* [Hello World](https://github.com/whoami-rx/http-go-examples/tree/simple_http_server)
-* [Multiplexer(mux)](https://github.com/whoami-rx/http-go-examples/tree/serve_multiplexer)
-* [Custom Handler](https://github.com/whoami-rx/http-go-examples/tree/custom-handler)
-* [Custom Server](https://github.com/whoami-rx/http-go-examples/tree/custom-server)
-* [Middleware](https://github.com/whoami-rx/http-go-examples/tree/middlewares)
-* [HTPP methods](https://github.com/whoami-rx/http-go-examples/tree/middlewares)
-* [Serve Static Files](https://github.com/whoami-rx/http-go-examples/tree/static-files)
+### GET
+
+```
+curl http://localhost:8080/user
+```
+
+### POST
+
+```
+curl http://localhost:8080/register -XPOST -d '{"email": "lorem@lorem", "password": "1235"}'
+```
--- server.go
+++ server.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// User ...
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// JSONMiddleware only set content type: application/json
+func JSONMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	fmt.Println("Hello, Gold!")
+	m := http.NewServeMux()
+	m.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(User{
+			Email:    "example@user.com",
+			Password: "NotSendPasswords",
+		})
+	})
+	m.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			var user User
+			json.NewDecoder(r.Body).Decode(&user)
+			fmt.Fprintf(w, "%q", user)
+		default:
+			w.Write([]byte("Try with post"))
+		}
+	})
+	log.Println("Server listen at :8080")
+	log.Fatal(http.ListenAndServe(":8080", JSONMiddleware(m)))
 }
